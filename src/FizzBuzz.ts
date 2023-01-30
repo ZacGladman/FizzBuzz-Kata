2323b3b5a1b37ab97f8a86d280f26e3662a36959
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-30 18:15:37 +0000

    detailed FizzBuzz pseudocode

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -14,3 +14,29 @@ function FizzBuzz(n):
     RETURN cleanedOutputString;
 END.
 
+
+==== More detailed solution ====
+
+function convertNumberOrKeepSame(inputNumber):
+    IF number is divisible by 3 AND 5:
+        return "FizzBuzz"
+    ELSE IF number is divisible by 3:
+        return "Fizz"
+    ELSE IF number is divisible by 5:
+        return "Buzz"
+    ELSE return number
+    END IF.
+END.
+
+function FizzBuzz(n):
+    Set rawOutputString equal to an empty string;
+    FOR EACH number from 1 to n:
+        Add a comma and a space onto the end of rawOutputString;
+        Set convertedNumber equal to convertNumberOrKeepSame(number);
+        Add convertedNumber onto the end of rawOutputString
+    END FOR.
+    Set cleanedOutputString to equal rawOutputString minus its first two characters (comma and space);
+    RETURN cleanedOutputString;
+END.
+
+*/
